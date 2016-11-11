8d94b570370091899b36ddf2ac88853373c57c7e
Warenga <warengam@gmail.com>
2016-11-11 15:06:11 +0300

    test SubClass Salary method

--- OOP/test_employee_class.py
+++ OOP/test_employee_class.py
@@ -9,11 +9,15 @@ class EmployeeClassTest(unittest.TestCase):
         mark = Employee(5,'Mark', 'Kenneth', 'markk@gmail.com' )
         self.assertIsInstance(mark, Employee, msg='The object should be an instance of the `Employee` class')
 
-    def test_Employee_Salary_method_1(self):
+    def test_Employee_Salary(self):
         mark = Employee(5,'Mark', 'Kenneth', 'markk@gmail.com' )
         self.assertEqual(mark.salary(), 0, msg='Salary should be able to be executed in the object')
 
     def test_PartTime_Sub_Class(self):
         Kelly = PartTime(4, 'Kelly', 'Howard', 'kelly@gmail.com')
         self.assertIsInstance(Kelly, PartTime, msg='The object should be an instance of "PartTime" class ')
+
+    def test_SubClass_Salary(self):
+        Kelly = PartTime(4, 'Kelly', 'Howard', 'kelly@gmail.com')
+        self.assertEqual(Kelly.salary(), 16000, msg='Salary should be able to be executed through an instance of the Sub Class')   
 
